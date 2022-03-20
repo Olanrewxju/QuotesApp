5d37fd18a0d836bbd0b96e2a0aba17269326932b
waju <mac@wajus-MacBook-Pro.local>
2022-03-20 04:28:41 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:motivateme/pages/home_page.dart';
+import 'package:motivateme/pages/intro_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quotes App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
